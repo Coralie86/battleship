53cc0531690fffffdf1c9bcce985a52fccfc8501
Coralie <207925650+Coralie86@users.noreply.github.com>
2026-01-04 21:27:50 +0100

    updating test file

--- src/testing/gameboard.test.js
+++ src/testing/gameboard.test.js
@@ -16,13 +16,13 @@ test('is a board', () => {
 
 test('place ship out of board', () => {
     let gameboard = new Gameboard();
-    expect(() => gameboard.placeShip(['A', 10], ['B', 11])).toThrow(Error);
+    expect(() => gameboard.placeShip([0, 10], [1, 11])).toThrow(Error);
 })
 
-test('place ship', () => {
+test('place ship - Board', () => {
     let gameboard = new Gameboard();
-    gameboard.placeShip(['A',1], ['C',1]); 
-    expect(gameboard.board).toEqual([[{"mark": 'X', "ship": {"hits": 0, "length": 3, "sunk": false}},{"mark": 'X', "ship": {"hits": 0, "length": 3, "sunk": false}},{"mark": 'X', "ship": {"hits": 0, "length": 3, "sunk": false}},'','','','','','',''],
+    gameboard.placeShip([0,0], 3, "horizontal"); 
+    expect(gameboard.board).toEqual([['X', 'X', 'X','','','','','','',''],
                                      ['','','','','','','','','',''],
                                      ['','','','','','','','','',''],
                                      ['','','','','','','','','',''],
@@ -36,44 +36,45 @@ test('place ship', () => {
 
 test('place ship', () => {
     let gameboard = new Gameboard();
-    gameboard.placeShip(['A',1], ['C',1]);
-    expect(gameboard.shipList).toEqual([{"ship": {"hits": 0, "length": 3, "sunk": false}, "coord": [['A',1], ['B',1], ['C',1]]}])
+    gameboard.placeShip([0,0], 3, "vertical");
+    expect(gameboard.shipList).toEqual([{"ship": {"hits": 0, "length": 3, "sunk": false, "direction": "vertical"}, "id":"ship-r0-c0", "coord": [[0,0], [1,0], [2,0]]}])
 })
 
 test('check if place already taken', () => {
     let gameboard = new Gameboard();
-    gameboard.placeShip(['A',2], ['C',2]);    
-    expect(() => gameboard.placeShip(['B',2], ['B',5])).toThrow(Error);
+    gameboard.placeShip([1,0], 3, "horizontal");    
+    expect(() => gameboard.placeShip([0,1], 3, "vertical")).toThrow(Error);
 })
 
 test('ship has been hit', () => {
     let gameboard = new Gameboard();
-    gameboard.placeShip(['A',1], ['C',1]);
-    gameboard.receiveAttack('B',1);
-    gameboard.receiveAttack('C',1);
-    expect(gameboard.board[0][1].ship.hits).toBe(2);
+    gameboard.placeShip([0,0], 3, "vertical");
+    gameboard.receiveAttack(1,0);
+    gameboard.receiveAttack(2,0);
+    let ship = gameboard.shipList.find((item) => item.id === "ship-r0-c0")
+    expect(ship.ship.hits).toBe(2);
 })
 
 test('missed hit', () => {
     let gameboard = new Gameboard();
-    gameboard.placeShip(['A',1], ['C',1]);
-    gameboard.receiveAttack('E',4);
-    expect(gameboard.board[3][4].mark).toBe('O');
+    gameboard.placeShip([0,0], 3, "horizontal");
+    gameboard.receiveAttack(4,3);
+    expect(gameboard.board[4][3]).toBe('O');
 })
 
-test('all ship sunk', () => {
+test('all ship sunk true', () => {
     let gameboard = new Gameboard();
-    gameboard.placeShip(['A',1], ['C',1]);
-    gameboard.receiveAttack('A',1);
-    gameboard.receiveAttack('B',1);
-    gameboard.receiveAttack('C',1);
+    gameboard.placeShip([0,0], 3, "vertical");
+    gameboard.receiveAttack(0,0);
+    gameboard.receiveAttack(1,0);
+    gameboard.receiveAttack(2,0);
     expect(gameboard.shipStatus()).toBeTruthy();
 })
 
-test('all ship sunk', () => {
+test('all ship sunk false', () => {
     let gameboard = new Gameboard();
-    gameboard.placeShip(['A',1], ['C',1]);
-    gameboard.receiveAttack('A',1);
-    gameboard.receiveAttack('C',1);
+    gameboard.placeShip([0,0], 3, "vertical");
+    gameboard.receiveAttack(0,0);
+    gameboard.receiveAttack(2,0);
     expect(gameboard.shipStatus()).not.toBeTruthy();
 })
